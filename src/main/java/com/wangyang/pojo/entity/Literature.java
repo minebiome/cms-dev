6795118264ec6c4d47396837f47a2c8f1311511f
WangYang <1749748955@qq.com>
2022-11-05 15:39:27 +0800

    update

--- src/main/java/com/wangyang/pojo/entity/Literature.java
+++ src/main/java/com/wangyang/pojo/entity/Literature.java
@@ -3,12 +3,14 @@ package com.wangyang.pojo.entity;
 import com.wangyang.pojo.entity.base.BaseEntity;
 import lombok.Data;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 
 @Entity(name = "t_literature")
 @Data
 public class Literature extends BaseEntity {
     private String title;
+    @Column(name = "literature_key")
     private String key;
     private String author;
     private String url;
