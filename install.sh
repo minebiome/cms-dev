37c3987094f68b6841ed46c0084c24b4a3b6725e
wangyang <1749748955@qq.com>
2023-01-07 13:00:53 +0800

    update

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+git pull
+mvn clean
+mvn install
+dir=`pwd`
+app="cms-dev-0.0.1-SNAPSHOT.jar"
+jar="${dir}/target/${app}"
+
+if [ ! -f $jar ];then
+  echo "build failure！！！"
+  exit 8
+fi
+
+pid=$(jps | grep $app | awk '{print $1}')
+
+if [ $pid ]
+then
+  echo "kill ${pid}"
+  kill -9 ${pid}
+fi
+
+echo $jar
+nohup java -jar $jar 2>&1 > bioinfo.log &
+tail -f bioinfo.log
