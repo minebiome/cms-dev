b21b745e05246af09bdd3170c8e2731867e59882
wangyang <1749748955@qq.com>
2023-06-26 11:37:44 +0800

    update

--- cms-boot/src/main/java/com/wangyang/CmsApplication.java
+++ cms-boot/src/main/java/com/wangyang/CmsApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -22,7 +21,7 @@ public class CmsApplication {
 
 	public static void main(String[] args) {
 		// Customize the spring config location
-		System.setProperty("spring.config.additional-location", "optional:file:${user.home}/cms/application.yml");
+		System.setProperty("spring.config.additional-location", "optional:file:${user.home}/cms1/application.yml");
 //		System.setProperty("spring.config.additional-location",
 //				"optional:file:${user.home}/cms/");
 		SpringApplication.run(CmsApplication.class, args);
---
+++ cms-boot/src/main/java/com/wangyang/Main.java
@@ -0,0 +1,7 @@
+package com.wangyang;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
