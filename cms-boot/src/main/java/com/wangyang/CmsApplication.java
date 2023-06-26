228c2d12f55b786583519ffdac3af81c303632ff
wangyang <1749748955@qq.com>
2023-06-26 12:58:08 +0800

    update

--- cms-boot/src/main/java/com/wangyang/CmsApplication.java
+++ cms-boot/src/main/java/com/wangyang/CmsApplication.java
@@ -22,7 +22,7 @@ public class CmsApplication {
 
 	public static void main(String[] args) {
 		// Customize the spring config location
-		System.setProperty("spring.config.additional-location", "optional:file:${user.home}/cms1/application.yml");
+		System.setProperty("spring.config.additional-location", "optional:file:${user.home}/cms/application.yml");
 //		System.setProperty("spring.config.additional-location",
 //				"optional:file:${user.home}/cms/");
 		SpringApplication.run(CmsApplication.class, args);
